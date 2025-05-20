1dd36abd76d27c93b704c9a2036122fdc93288ca
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-05-20 20:40:40 +0000

    feat: budget requests

---
+++ yearn_treasury/budget/__init__.py
@@ -0,0 +1,5 @@
+from yearn_treasury.budget._request import BudgetRequest
+from yearn_treasury.budget._requests import approved_requests, rejected_requests, requests
+
+
+__all__ = ["BudgetRequest", "requests", "approved_requests", "rejected_requests"]
