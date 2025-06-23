b1ff9b1e58e6e7c2dc65d1c2c0cbbf78502bb382
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-06-23 18:00:18 -0400

    feat: sort cowswap swaps (#128)

--- yearn_treasury/rules/ignore/swaps/__init__.py
+++ yearn_treasury/rules/ignore/swaps/__init__.py
@@ -8,6 +8,7 @@ swaps: Final[SortRuleFactory[IgnoreSortRule]] = ignore("Swaps")
 
 from yearn_treasury.rules.ignore.swaps.aave import *
 from yearn_treasury.rules.ignore.swaps.compound import *
+from yearn_treasury.rules.ignore.swaps.cowswap import *
 from yearn_treasury.rules.ignore.swaps.gearbox import *
 from yearn_treasury.rules.ignore.swaps.pooltogether import *
 from yearn_treasury.rules.ignore.swaps.synthetix import *
