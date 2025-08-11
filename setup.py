e770b3c24cb7f41db0d52bbc99ce6deb602b8342
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-08-11 14:39:29 -0400

    fix: compilation err in curve sort rules (#198)

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ else:
             "yearn_treasury/rules/expense",
             # "yearn_treasury/rules/ignore",
             "yearn_treasury/rules/ignore/swaps/conversion_factory.py",
-            "yearn_treasury/rules/ignore/swaps/curve.py",
+            # "yearn_treasury/rules/ignore/swaps/curve.py",  push async-lru-c first
             "yearn_treasury/rules/ignore/swaps/gearbox.py",
             "yearn_treasury/rules/ignore/swaps/iearn.py",
             "yearn_treasury/rules/ignore/swaps/otc.py",
