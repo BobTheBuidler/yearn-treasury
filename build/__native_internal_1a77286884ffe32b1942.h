74157dc1654b0beaf20d5ac1bd7bac6cb23e6ee8
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-08-23 23:18:23 -0400

    fix: decimals rounding in woofy sort rules (#210)
    * fix: woofy scaling
    
    * chore: `black .`
    
    * chore: compile C files for source control
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- build/__native_internal_1a77286884ffe32b1942.h
+++ build/__native_internal_1a77286884ffe32b1942.h
@@ -6,7 +6,7 @@
 
 int CPyGlobalsInit(void);
 
-extern PyObject *CPyStatics[760];
+extern PyObject *CPyStatics[769];
 extern const char * const CPyLit_Str[];
 extern const char * const CPyLit_Bytes[];
 extern const char * const CPyLit_Int[];
