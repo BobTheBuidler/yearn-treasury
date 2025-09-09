8423ad8a5f5e8a8932228ca2e3a637afea871e74
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-09-09 01:08:13 -0400

    chore: suppress error logs for SAFE (#223)
    * chore: suppress error logs for SAFE
    
    * chore: compile C files for source control
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- build/__native_internal_0e100735679f22f49703.h
+++ build/__native_internal_0e100735679f22f49703.h
@@ -6,7 +6,7 @@
 
 int CPyGlobalsInit(void);
 
-extern PyObject *CPyStatics[780];
+extern PyObject *CPyStatics[781];
 extern const char * const CPyLit_Str[];
 extern const char * const CPyLit_Bytes[];
 extern const char * const CPyLit_Int[];
