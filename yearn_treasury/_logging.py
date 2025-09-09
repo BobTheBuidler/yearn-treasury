e101331c1530523257631711caaeb22e1600e360
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-09-09 04:19:33 +0000

    feat: implement SUPPRESS_ERROR_LOGS

---
+++ yearn_treasury/_logging.py
@@ -0,0 +1,19 @@
+"""
+This file contains logic for suppressing eth-portfolio error logs where desired.
+"""
+
+from cchecksum import to_checksum_address
+from eth_portfolio._utils import SUPPRESS_ERROR_LOGS
+from y import Network
+
+from yearn_treasury.constants import CHAINID
+
+
+suppress_logs_for = {
+    Network.Mainnet: [
+    ],
+}
+
+def setup_eth_portfolio_logging() -> None:
+    for token in suppress_logs_for.get(CHAINID, []):
+        SUPPRESS_ERROR_LOGS.append(to_checksum_address(token))
