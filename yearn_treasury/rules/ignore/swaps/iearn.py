923189557f124bcffaba10c547e6d09b33ccd46e
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2026-01-24 23:16:49 -0400

    fix: checksum iEarn pool addresses to reduce mypy dict-item errors

--- yearn_treasury/rules/ignore/swaps/iearn.py
+++ yearn_treasury/rules/ignore/swaps/iearn.py
@@ -1,5 +1,6 @@
 from typing import Final, cast
 
+from cchecksum import to_checksum_address
 from dao_treasury import TreasuryTx
 from eth_typing import ChecksumAddress
 from y import Contract, Network
@@ -10,17 +11,17 @@ from yearn_treasury.rules.ignore.swaps import swaps
 IEARN: Final[dict[str, ChecksumAddress]] = {
     # v1 - deprecated
     # v2
-    "yDAIv2": "0x16de59092dAE5CcF4A1E6439D611fd0653f0Bd01",
-    "yUSDCv2": "0xd6aD7a6750A7593E092a9B218d66C0A814a3436e",
-    "yUSDTv2": "0x83f798e925BcD4017Eb265844FDDAbb448f1707D",
-    "ysUSDv2": "0xF61718057901F84C4eEC4339EF8f0D86D2B45600",
-    "yTUSDv2": "0x73a052500105205d34daf004eab301916da8190f",
-    "yWBTCv2": "0x04Aa51bbcB46541455cCF1B8bef2ebc5d3787EC9",
+    "yDAIv2": to_checksum_address("0x16de59092dAE5CcF4A1E6439D611fd0653f0Bd01"),
+    "yUSDCv2": to_checksum_address("0xd6aD7a6750A7593E092a9B218d66C0A814a3436e"),
+    "yUSDTv2": to_checksum_address("0x83f798e925BcD4017Eb265844FDDAbb448f1707D"),
+    "ysUSDv2": to_checksum_address("0xF61718057901F84C4eEC4339EF8f0D86D2B45600"),
+    "yTUSDv2": to_checksum_address("0x73a052500105205d34daf004eab301916da8190f"),
+    "yWBTCv2": to_checksum_address("0x04Aa51bbcB46541455cCF1B8bef2ebc5d3787EC9"),
     # v3
-    "yDAIv3": "0xC2cB1040220768554cf699b0d863A3cd4324ce32",
-    "yUSDCv3": "0x26EA744E5B887E5205727f55dFBE8685e3b21951",
-    "yUSDTv3": "0xE6354ed5bC4b393a5Aad09f21c46E101e692d447",
-    "yBUSDv3": "0x04bC0Ab673d88aE9dbC9DA2380cB6B79C4BCa9aE",
+    "yDAIv3": to_checksum_address("0xC2cB1040220768554cf699b0d863A3cd4324ce32"),
+    "yUSDCv3": to_checksum_address("0x26EA744E5B887E5205727f55dFBE8685e3b21951"),
+    "yUSDTv3": to_checksum_address("0xE6354ed5bC4b393a5Aad09f21c46E101e692d447"),
+    "yBUSDv3": to_checksum_address("0x04bC0Ab673d88aE9dbC9DA2380cB6B79C4BCa9aE"),
 }
 
 POOLS: Final = set(IEARN.values())
